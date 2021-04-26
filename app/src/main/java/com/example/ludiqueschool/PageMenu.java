5ab94276d8ad3ca5b6cfdf0a8d9e3a19c6ba5b96
haikelJebahi <haikel.jebahi@hotmail.com>
2021-04-26 14:57:18 +0200

    Page de connexion + page menu

---
+++ app/src/main/java/com/example/ludiqueschool/PageMenu.java
@@ -0,0 +1,17 @@
+package com.example.ludiqueschool;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class PageMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_menu);
+    }
+}
